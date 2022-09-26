09687f8abd6aea2738b6a57271a6ac69717f786e
Andrea Miglietta <m.andrea96@hotmail.it>
2022-09-26 19:03:15 +0200

    esercizio

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ saluto }}</p>
+    <div v-for="(item, index) in test" :key="index">
+      {{ item }}
+      <FirstComponent />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FirstComponent from "./components/first-component.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      saluto: "Hi to everybody",
+      test: [1, 3, 6, 9, 12, 1],
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    FirstComponent,
+  },
+};
 </script>
 
 <style lang="scss">
---
+++ src/components/first-component.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="red" @click="saluta">Primo vue component</div>
+</template>
+
+<script>
+export default {
+  name: "FirstComponent",
+  methods: {
+    saluta() {
+      console.log("saluta il tuo amico");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.red {
+  background-color: red;
+}
+</style>
